09b989ad7e01661b4dd11e6b9aa4326387e558d6
yogeshmalkani-pillup <conan.yogesh@gmail.com>
2024-07-02 17:19:44 +0530

    changes

--- src/components/home/InfluencerCard.js
+++ src/components/home/InfluencerCard.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function InfluencerCard({imageName, quote}) {
+export default function InfluencerCard({imageName, quote, key}) {
   return (
-    <div className='w-[25%] max-md:w-[40%] max-sm:w-full rounded-[58px] bg-white bg-opacity-10 py-10 px-5  flex flex-col justify-between items-stretch'>
+    <div key={key} className='w-[25%] max-md:w-[40%] max-sm:w-full rounded-[58px] bg-white bg-opacity-10 py-10 px-5  flex flex-col justify-between items-stretch'>
         <img className='h-[70%] object-contain'  src={`images/${imageName}`}/>
         <p className='font-satoshi_regular text-[1.125vw] max-md:text-[18px] '>{quote}</p>
     </div>
