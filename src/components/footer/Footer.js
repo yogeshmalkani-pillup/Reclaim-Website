6487012452c7444b02309c32f3bc9635f7480ddd
iamyogeshmalkani <conan.yogesh@gmail.com>
2024-07-11 22:03:18 +0530

    changes

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,51 +1,50 @@
 import React from 'react'
 import { Mulish } from "next/font/google";
 import { HOME, TERMSANDCONDITION } from '@/utils/Contants';
-import Home from '@/pages';
 const mulish = Mulish({ subsets: ['latin'] })
 
 export default function Footer({ setActivePage }) {
     return (
         <div className='w-full'>
             <div className={`w-full flex flex-row gap-10 flex-wrap justify-between px-20 py-28 max-md:p-10 max-sm:p-5 bg-[#111312] ${mulish.className}`}>
-            <div className='flex flex-col gap-10 max-sm:gap-5 max-md:w-full ' onClick={()=> setActivePage(HOME)}>
-                <div className='flex flex-row items-center gap-2'>
-                    <img className='w-[100px] h-[100px] max-sm:w-[50px] max-sm:h-[50px]'
-                        src="/icons/reclaim-logo.svg"
-                        alt="recalim-logo"
-                    />
-                    <p className='text-[36px] max-sm:text-[18px] font-bold font-satoshi_bold'>Reclaim</p>
+                <div className='flex flex-col gap-10 max-sm:gap-5 max-md:w-full ' >
+                    <div className='flex flex-row items-center gap-2' onClick={() => setActivePage(HOME)}>
+                        <img className='w-[100px] h-[100px] max-sm:w-[50px] max-sm:h-[50px]'
+                            src="/icons/reclaim-logo.svg"
+                            alt="recalim-logo"
+                        />
+                        <p className='text-[36px] max-sm:text-[18px] font-bold font-satoshi_bold'>Reclaim</p>
+                    </div>
+                    <p className='text-[18px] max-sm:text-[15px] w-[60%] max-md:w-full'>Make the most out of every day with the worlds greatest Screen Time software.</p>
                 </div>
-                <p className='text-[18px] max-sm:text-[15px] w-[60%] max-md:w-full'>Make the most out of every day with the worlds greatest Screen Time software.</p>
-            </div>
 
-            <div className='flex flex-col gap-10 '>
-                <p className='text-[24px] max-sm:text-[20px] font-bold'>Other</p>
-                <div className='flex flex-col gap-2'>
-                    <p className='text-[18px] max-sm:text-[15px] font-semibold cursor-pointer'>FAQs</p>
-                    <p className='text-[18px] max-sm:text-[15px] font-semibold cursor-pointer'>Privacy Policy</p>
-                    <p className='text-[18px] max-sm:text-[15px] font-semibold cursor-pointer' onClick={() => setActivePage(TERMSANDCONDITION)}>Terms of Use</p>
+                <div className='flex flex-col gap-10 '>
+                    <p className='text-[24px] max-sm:text-[20px] font-bold'>Other</p>
+                    <div className='flex flex-col gap-2'>
+                        <p className='text-[18px] max-sm:text-[15px] font-semibold cursor-pointer'>FAQs</p>
+                        <p className='text-[18px] max-sm:text-[15px] font-semibold cursor-pointer'>Privacy Policy</p>
+                        <p className='text-[18px] max-sm:text-[15px] font-semibold cursor-pointer' onClick={() => setActivePage(TERMSANDCONDITION)}>Terms of Use</p>
+                    </div>
                 </div>
-            </div>
 
-            <div className='flex flex-col gap-10'>
-                <p className='text-[24px] font-bold max-sm:h-[18px]'>Follow us</p>
-                <div className='flex flex-row gap-10'>
-                    <img className='w-[36px] h-[36px] max-sm:w-[20px] max-sm:h-[20px]' src='icons/facebook.svg' />
-                    <img className='w-[36px] h-[36px] max-sm:w-[20px] max-sm:h-[20px]' src='icons/twitter.svg' />
-                    <img className='w-[36px] h-[36px] max-sm:w-[20px] max-sm:h-[20px]' src='icons/linkedin.svg' />
+                <div className='flex flex-col gap-10'>
+                    <p className='text-[24px] font-bold max-sm:h-[18px]'>Follow us</p>
+                    <div className='flex flex-row gap-10'>
+                        <img className='w-[36px] h-[36px] max-sm:w-[20px] max-sm:h-[20px]' src='icons/facebook.svg' />
+                        <img className='w-[36px] h-[36px] max-sm:w-[20px] max-sm:h-[20px]' src='icons/twitter.svg' />
+                        <img className='w-[36px] h-[36px] max-sm:w-[20px] max-sm:h-[20px]' src='icons/linkedin.svg' />
+                    </div>
                 </div>
+
             </div>
-          
+            <footer>
+                <div className='w-full h-[10vh] overflow-hidden'>
+                    <video playsInline autoPlay loop muted className='w-full'  >
+                        <source src={`/videos/galaxy.mp4`} type="video/mp4" />
+                    </video>
+                </div>
+            </footer>
         </div>
-        <footer>
-            <div className='w-full h-[10vh] overflow-hidden'>
-                <video playsInline autoPlay loop muted className='w-full'  >
-                    <source src={`/videos/galaxy.mp4`} type="video/mp4" />
-                </video>
-            </div>
-        </footer>
-    </div>
-      
+
     )
 }
